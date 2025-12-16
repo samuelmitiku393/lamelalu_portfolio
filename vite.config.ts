592bb71df2bf12628e312fd76dcfdef53be81f6a
Samuel Mitiku <samuelmitku393@gmail.com>
2025-12-16 11:04:02 +0300

    updated the package.json and other config files

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          animations: ['framer-motion'],
-        }
-      }
-    }
-  },
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript',
-    }
+    sourcemap: false
   }
 })
