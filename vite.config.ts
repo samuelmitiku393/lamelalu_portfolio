70d350ad841d2dc9c13b3a6242d21cdaf5bb29f2
Samuel Mitiku <samuelmitku393@gmail.com>
2025-12-16 10:58:23 +0300

    trying to make it work on netlify

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          animations: ['framer-motion'],
+        }
+      }
+    }
+  },
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript',
+    }
+  }
+})
